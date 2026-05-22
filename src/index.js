ad317ef6273e54968e9d245b6e327078b80e947b
Noah Gullo <noahgulloingram@gmail.com>
2026-05-22 07:15:47 -0700

    chore: add rudimentary class setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Node{
+    constructor(){
+        this.data = null;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree{
+    constructor(array){
+        this.root = buildTree(array);
+    }
+
+    #buildTree(array){
+        return null;
+    }
+}
